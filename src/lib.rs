18b1ea5e5e93f8a3659e6a8a33bf066aa23e7071
Jonathan Reem <jonathan.reem@gmail.com>
2014-07-27 23:42:25 -0700

    (cleanup) Cleanup and reform API.

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,33 @@
- #![license = "MIT"]
- #![deny(missing_doc)]
- #![deny(unnecessary_qualification, non_camel_case_types,
-         unused_variable, unnecessary_typecast)]
+#![license = "MIT"]
+#![feature(phase)]
+#![deny(missing_doc)]
+#![deny(unnecessary_qualification, non_camel_case_types,
+        unused_variable, unnecessary_typecast)]
 
 //! Parser for HTTP priority headers such as Accept-Encoding
+
+extern crate regex;
+#[phase(plugin, link)] extern crate regex_macros;
+
+use regex::Regex;
 
 static VALID_HEADER_ITEM: Regex = regex!(r"/^\s*(\S+?)\s*(?:;(.*))?$/");
 static WHITESPACE: &'static [char] = &[' ', '\t', '\n'];
 
 pub fn parse_priorities_for<S: Str>(header: &str, candidates: Vec<S>) -> Vec<(S, f64)> {
-    use parse::parse_header;
-    use match::priorities_for;
+    use parser::parse_header;
+    use matcher::priorities_for;
 
     let priorities = parse_header(header);
-    priorities_for(&priorities, candidates)
-        .filter(&(_, p) p > 0.0)
+    priorities_for(&priorities, candidates).move_iter()
+        .filter(|&(_, p)| p > 0.0)
         .collect()
 }
 
-pub mod parse {
+pub mod parser {
+    use super::{VALID_HEADER_ITEM, WHITESPACE};
+    use std::collections::HashMap;
+
     pub fn parse_header<'a>(header: &'a str) -> HashMap<&'a str, f64> {
         header
             .trim_chars(WHITESPACE)
@@ -43,7 +52,7 @@ pub mod parse {
             }).map_or(1.0, |param| {
 
                 // Found q, parse its value
-                from_str::<f64>(param.trim_chars(whitespace)
+                from_str::<f64>(param.trim_chars(WHITESPACE)
                     .split('=')
                     .collect::<Vec<&str>>()[1])
 
@@ -55,15 +64,18 @@ pub mod parse {
     }
 }
 
-pub mod match {
+pub mod matcher {
+    use std::collections::HashMap;
     use std::vec::MoveItems;
+    use std::iter::Map;
 
     pub fn priorities_for<S: Str>(accepts: &HashMap<&str, f64>,
-                                  provided: Vec<S>) -> MoveItems<(S, f64)> {
+                                  provided: Vec<S>) -> Vec<(S, f64)> {
+        let error = -1.0;
         provided.move_iter().map(|value| {
-            let &priority = accepts.find_equiv(&possibility.as_slice()).unwrap_or(&error);
-            (possibility, priority)
-        })
+            let &priority = accepts.find_equiv(&value.as_slice()).unwrap_or(&error);
+            (value, priority)
+        }).collect()
     }
 }
 
